9cc8a27ee3bd73f14de57f67d641e782f444e1d7
Josh Ramirez <joshuaram.21950@redlandsschools.net>
2018-09-17 15:25:38 +0000

    testing

---
+++ amazing.py
@@ -0,0 +1,16 @@
+"""
+This  program demonstrates IMPORT and LOOPS 
+"""
+
+# import statements go FIRST! (TOP OF YOUR CODE)
+import time 
+
+# for variableName in range (start, stop , step)
+#OR
+# for variableName in range (stop): * this will start at 0 and step by 1
+
+for Josh in range (20, -5, -2):
+    print('hello' + str(Josh))
+
+# count to 100 by 5's
+# count down from (START AT 20) by 2's and stop at -4
