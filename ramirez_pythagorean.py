0b8c69d2faf08142cfdefaa4aebaef9c40a0cba7
Josh Ramirez <joshuaram.21950@redlandsschools.net>
2018-09-17 05:22:23 +0000

    put in pythagorean

---
+++ ramirez_pythagorean.py
@@ -0,0 +1,12 @@
+"""
+Wite a program that will ask the user to enter their name and respond with a greeting using this name.
+"""
+name = input('Enter your name: ')
+print("hello", name)
+
+x = input('please enter the lenghts of the legs of a right triangle:')
+a = float(input("a: "))
+b = float(input("b: "))
+
+c = sqrt(a**2 + b**2)
+print("The length of the hypotenuse is", c )
